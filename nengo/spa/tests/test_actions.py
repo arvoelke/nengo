f8b181e10cea6f99b75e8004d75ce20bb6254bf6
Jan Gosmann <jgosmann@uwaterloo.ca>
2017-11-14 18:17:50 -0500

    fixup! Filter irrelevant warnings.

--- nengo/spa/tests/test_actions.py
+++ nengo/spa/tests/test_actions.py
@@ -104,7 +104,7 @@ def test_action():
                    'motor=A, motor2=B', name='test_action')
 
 
-@pytest.mark.filterwarnings('ignore:.*must be processed')
+@pytest.mark.filterwarnings('ignore:The actions currently added')
 def test_actions():
     a = Actions(
         'dot(state, A) --> state=B',
